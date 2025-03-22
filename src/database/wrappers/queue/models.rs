39518a6a0b185bf2f8cfcecd8d07295710b63ebf
ehewes <ehewes@outlook.com>
2025-03-22 22:38:28 +0000

    fix

--- src/database/wrappers/queue/models.rs
+++ src/database/wrappers/queue/models.rs
@@ -11,3 +11,29 @@ pub struct Webhook {
     pub discord_webhook_id: i64,
     pub banned: bool,
 }
+
+#[derive(Insertable, Debug)]
+#[diesel(table_name = schema::webhooks)]
+pub struct newWebhook {
+    pub discord_webhook_id: i64,
+    pub banned: bool,
+}
+
+#[derive(AsChangeset, Debug)]
+#[diesel(table_name = schema::webhooks)]
+pub struct updateWebhook {
+    pub discord_webhook_id: Option<i64>,
+    pub banned: Option<bool>,
+}
+
+#[derive(Debug, PartialEq, Eq, FromSqlRow, AsExpression)]
+#[diesel(sql_type = "Text")]
+pub enum Status {
+    Pending,
+    Completed
+}
+
+
+
+
+
